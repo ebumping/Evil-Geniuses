e07858c81dec2c6e9b6ce982c45887088b9576fa
ebumping <ebumpingxkcd@gmail.com>
2016-11-06 21:15:19 -0500

    fixed button

--- EvilGeniuses/src/sideWork/RandomCode.java
+++ EvilGeniuses/src/sideWork/RandomCode.java
@@ -1,5 +1,13 @@
 package sideWork;
 
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Synthesizer;
+
 public class RandomCode {
+	public static void main(String[] args) {        
+		Synthesizer synth = MidiSystem.getSynthesizer();
+		synth.open();           
+	}
+	
 
 }
